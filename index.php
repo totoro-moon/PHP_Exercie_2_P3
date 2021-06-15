9d020176aba42e3c9d4412cc3b4ef3fa81c2a6c3
totoro-moon <flaviefrancois21@gmail.com>
2021-06-15 13:27:13 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercice 2 Partie 3</title>
+</head>
+<body>
+    <h1># Exercice 2</h1>
+<p>Créez une fonction qui retourne une chaîne de caractère
+</p>
+<?php
+$text='hello you!';
+function phrase ($string ='coucou'){
+    return $string;
+}
+echo phrase($text);
+?>
+</body>
+</html>
